1be729243a82799cb384a1faa73913ef1406a1cf
Tiago Pieroni <tepieroni@gmail.com>
2023-05-15 17:51:03 -0300

    Agregar archivos de proyecto.

---
+++ Geometria/Poligono.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometria
+{
+    public class Poligono 
+    {
+        public void CalcularPerimetro()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CalcularSuperficie()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class Rectangulo : Poligono {}
+}
---
+++ Geometria/Rectangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometria
+{
+    public class Rectangulo
+    {
+    }
+    
+    public class Cuadrado : Rectangulo { }
+}
